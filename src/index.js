58d332d3e07ba119fa9bff4117871f38b5f2b812
rmahawewa <rmahawewa@yahoo.com>
2025-01-15 12:47:09 +0530

    select individual student record to edit

--- src/index.js
+++ src/index.js
@@ -16,8 +16,6 @@ student_add.addEventListener("click", function(){
     content.innerHTML = add_Student;
 });
 
-
-
 // let student_info = student_info;
 student_edit_form.addEventListener("click", function(){ //json_inforation
     let form = student_edit(student_info[0]).student_edit_form;
@@ -29,4 +27,16 @@ student_view.addEventListener("click", function(){
     // console.log(student_info);
     let table_data = studentView(student_info).student_view_table;
     content.innerHTML = table_data;
+});
+
+
+content.addEventListener("click", function(e){
+
+    if(e.target.getAttribute("class") !== null && e.target.getAttribute("class").toString().localeCompare("edit") === 0 ){
+        let id = e.target.getAttribute("id");
+        let index = Number(id) - 1;
+        let form = student_edit(student_info[index]).student_edit_form;
+        content.innerHTML = form;
+    }
+
 });
